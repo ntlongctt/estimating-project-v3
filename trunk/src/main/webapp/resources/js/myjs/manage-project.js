6e80ff0446d84eb9d51ce4dc9c7a0892e5ff7764
vuhung1711@gmail.com <vuhung1711@gmail.com@c606b4f5-0053-8777-99f7-aa91c2b32b92>
2014-11-26 01:07:06 +0000

    Update manage-project

--- trunk/src/main/webapp/resources/js/myjs/manage-project.js
+++ trunk/src/main/webapp/resources/js/myjs/manage-project.js
@@ -1,5 +1,8 @@
 $(document).ready(
 function() {
+editTableUCP();
+selectUCP();
+
 $("#projectTable > tbody").html("");
 
 $.getJSON("listprojectjson.json",{ajax : 'true'},
@@ -20,7 +23,70 @@ $.getJSON("listprojectjson.json",{ajax : 'true'},
 	})
 })
 
-// 
+// Event collapse for UCP
+function editTableUCP() {
+	$("#editUCPWeight").click(function() {
+		var value = document.getElementById("editUCPWeight").innerHTML;
+		if (value == "Edit") {
+			$("#panelUCPWeight").slideDown("slow");
+			document.getElementById("editUCPWeight").innerHTML = "Hide";
+		}
+		if (value == "Hide") {
+			$("#panelUCPWeight").slideUp("slow");
+			document.getElementById("editUCPWeight").innerHTML = "Edit";
+		}
+	});
+
+	$("#editFPWeigth").click(function() {
+		var value = document.getElementById("editFPWeigth").innerHTML;
+		if (value == "Edit") {
+			$("#panelFPWeigth").slideDown("slow");
+			document.getElementById("editFPWeigth").innerHTML = "Hide";
+		}
+		if (value == "Hide") {
+			$("#panelFPWeigth").slideUp("slow");
+			document.getElementById("editFPWeigth").innerHTML = "Edit";
+		}
+	});
+
+	$("#editTCF").click(function() {
+		var value = document.getElementById("editTCF").innerHTML;
+		if (value == "Edit") {
+			$("#panelTCF").slideDown("slow");
+			document.getElementById("editTCF").innerHTML = "Hide";
+		}
+		if (value == "Hide") {
+			$("#panelTCF").slideUp("slow");
+			document.getElementById("editTCF").innerHTML = "Edit";
+		}
+	});
+
+	$("#editIFC").click(function() {
+		var value = document.getElementById("editIFC").innerHTML;
+		if (value == "Edit") {
+			$("#panelIFC").slideDown("slow");
+			document.getElementById("editIFC").innerHTML = "Hide";
+		}
+		if (value == "Hide") {
+			$("#panelIFC").slideUp("slow");
+			document.getElementById("editIFC").innerHTML = "Edit";
+		}
+	});
+
+	$("#editPayment").click(function() {
+		var value = document.getElementById("editPayment").innerHTML;
+		if (value == "Edit") {
+			$("#panelPayment").slideDown("slow");
+			document.getElementById("editPayment").innerHTML = "Hide";
+		}
+		if (value == "Hide") {
+			$("#panelPayment").slideUp("slow");
+			document.getElementById("editPayment").innerHTML = "Edit";
+		}
+	});
+}
+
+// View version for ucp and fp when click Detail in Table List Project 
 function viewVersion(projectID) {
 	$.getJSON(projectID + "/viewDetailFpUc.json", {ajax : 'true'}, 
 	function(data) {
@@ -34,6 +100,15 @@ function viewVersion(projectID) {
 			    text: lstFp[i].tenProject,
 			}));
 		}
-		document.getElementById("projectName").innerHTML = projectName;
+		document.getElementById("projectName").innerHTML = "Project : " + "<b style='color:#3c8dbc'>" + projectName + "</b>"; 
 	})
+}
+
+function selectUCP() {
+	$('#selectUCP').change(function () {
+	    var optionSelected = $(this).find("option:selected");
+	    var valueSelected  = optionSelected.val();
+	    var textSelected   = optionSelected.text();
+	    alert("Value: " + valueSelected +" Text: " +textSelected);
+	});
 }
